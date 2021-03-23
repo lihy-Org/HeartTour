427129de4a43884ced4110532220f68c5ab5c035
chenlinhao <gladius777@163.com>
2021-03-23 19:03:01 +0800

    排班

--- src/FrontEnd/Store/src/main.js
+++ src/FrontEnd/Store/src/main.js
@@ -20,6 +20,8 @@ import './utils/error-log' // error log
 
 import * as filters from './filters' // global filters
 
+import _ from 'lodash'
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -37,6 +39,8 @@ Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
 })
+
+Vue.prototype.$_ = _
 
 // register global utility filters
 Object.keys(filters).forEach(key => {
